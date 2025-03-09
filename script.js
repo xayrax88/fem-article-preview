9173c6dfc6d828dd262c1a25012b04cba9387ba9
Anouxay Sichanh <ainthaly@gmail.com>
2025-03-09 15:50:01 -0400

    project finished

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const socialLinks = document.querySelector('.social-links');
 
 
 shareBtn.addEventListener('click', () => {
-    // socialLinks.style.display = (socialLinks.style.display === "flex") ? "none" : "flex";
     socialLinks.classList.toggle('active');
-    // shareBtn.classList.toggle('share-active')
+
     if (shareBtn.classList.contains('share-active')) {
-        shareBtn.classList.toggle.remove('share-active', 'share-active-img');
+        shareBtn.classList.remove('share-active', 'share-active-img');
     } else {
         shareBtn.classList.add('share-active', 'share-active-img');
+        console.log(shareBtn.classList);
+
     }
 })
